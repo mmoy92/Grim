9291b17c7022b2aa66b0a0dec6503aef533ddb15
David Odell <dodell11@vt.edu>
2014-04-07 15:36:55 -0400

    Player Inventory/Key and Door
    Added scripts to setup a player inventory which right now will
    determine if a player has a key which allows them to open a door. Still
    need to figure out if door reappears when passed through

---
+++ Assets/Advanced 2.5D Platformer Kit/Scripts/PlayerInventory.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInventory : MonoBehaviour {
+
+	public bool hasKey1;	//Whether or not the player has the first key
+	public bool usedKey = false;
+}
