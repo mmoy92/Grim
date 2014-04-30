0dc2af0848b806702ff433e6ac0c0a7fe2afce8e
stef5 <stef5@vt.edu>
2014-04-30 17:29:50 -0400

    level sound changes

--- Assets/AnimSounds.cs
+++ Assets/AnimSounds.cs
@@ -8,7 +8,10 @@ public class AnimSounds : MonoBehaviour {
 	public AudioClip slashClip;
 	public AudioClip evilDashClip; 
 	public AudioClip goodDashClip; 
+	public AudioClip evilAttackSound;
+	public AudioClip goodAttackSound;
 	public AudioClip footstepsClip;
+	public GameObject camera; // get the level sounds from the camera
 	public float footstepsDistance = 2.0f;
 
 	public float volume = 1.0f;
@@ -16,11 +19,24 @@ public class AnimSounds : MonoBehaviour {
 	private bool onGround = true;
 	private bool walking = false;
 	private PlatformerPhysics platformer;
+	private AudioSource[] audios;
 
 	public void Start()
 	{
 		platformer = GetComponent<PlatformerPhysics>();
+		audios = camera.GetComponents<AudioSource>();
 		StartCoroutine(footsteps());
+		if (audios.Length == 1)
+		{
+			audios[0].loop = true;
+			audios[0].Play();
+		} else if (audios.Length == 4) {
+			// when there are 4 background tracks
+			for (int i = 0; i < 4; i++) {
+				audios[i].mute = true;
+				audios[i].Play();
+			}
+		}
 	}
 	
 	public IEnumerator footsteps()
@@ -39,6 +55,21 @@ public class AnimSounds : MonoBehaviour {
 
 	}
 	
+	public void pickupKey(int key)
+	{
+		if (audios.Length == 4)
+		{
+			audios[key - 1].mute = false;
+		}
+	}
+	void StartEvilSplode()
+	{
+		AudioSource.PlayClipAtPoint(evilAttackSound, transform.position, volume);
+	}
+	void StartGoodSpear()
+	{
+		AudioSource.PlayClipAtPoint(goodAttackSound, transform.position, volume);
+	}
 	void StartedEvilDash()
 	{
 		AudioSource.PlayClipAtPoint(evilDashClip, transform.position, volume);
--- Assets/Scripts/KeyPickup.cs
+++ Assets/Scripts/KeyPickup.cs
@@ -5,13 +5,14 @@ public class KeyPickup : MonoBehaviour {
 
 	private GameObject player;                      // Reference to the player.
 	private grimInfo griminfo;        // Reference to the player's inventory.
-	
+	private AnimSounds sounds;		  // Reference to sound controller.
 	
 	void Awake ()
 	{
 		// Setting up the references.
 		player = GameObject.FindGameObjectWithTag("Player");
 		griminfo = player.GetComponent<grimInfo>();
+		sounds = player.GetComponent<AnimSounds>();
 	}
 	
 	
@@ -21,7 +22,7 @@ public class KeyPickup : MonoBehaviour {
 		if (other.gameObject == player) {
 
 			griminfo.keys += 1;
-			
+			sounds.pickupKey(griminfo.keys);
 			// Destroy this gameobject
 			Destroy (gameObject);
 		}
