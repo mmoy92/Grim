c9a6a0e2ff7990643d200f11a07cc096b71e5f80
ngrippin <ngrippin@vt.edu>
2014-05-04 16:37:28 -0400

    Fireball + Boss script start
    Rough working boss script and tracking fireballs script (tested)

---
+++ Assets/Scripts/BossScript.js
@@ -0,0 +1,79 @@
+﻿#pragma strict
+
+var target : Transform;
+var fireball : Transform;
+var bat : Transform;
+
+var health: int = 10;
+var attackDistance = 8.0;
+var attackTimer = Time.time;
+var attackDif = 5f;
+var attackMode: int = 0;
+var stage1 : int = 0;
+var stage2 : int = 0;
+
+private var transf : Transform;
+private var character: CharacterController;
+
+function Start () {
+	if (!target) target = GameObject.FindWithTag ("Player").transform; 
+	transf = transform;
+    character = GetComponent(CharacterController);
+}
+
+function getHurt(amt:int)
+{
+	health -= amt;	
+}
+
+function Update () {
+	if(health <= 0)
+	{
+		Die();
+	}
+	else if (target){
+		var tgtDir = target.position - transf.position;
+        var tgtDist = tgtDir.magnitude;
+        //Stage 1 - 1 bat, start fireballs
+        if(tgtDist < attackDistance)
+        {
+        	if(attackMode == 0)
+        	{
+        		Instantiate (bat, transform.position, Quaternion.identity);
+        	}
+        	attackMode = 1;
+        }
+        if(attackMode == 1)
+        {
+        	
+        	if(Time.time - attackTimer > attackDif)
+        	{
+        		Instantiate (fireball, transform.position, Quaternion.identity);
+        		attackTimer = Time.time;
+        	}
+        	//Stage 2 - 2 bats, fast fireballs
+        	if(health <= 7 && stage1 == 0)
+        	{
+        		Instantiate (bat, transform.position, Quaternion.identity);
+        		Instantiate (bat, transform.position, Quaternion.identity);
+        		stage1 = 1;
+        		//Fire faster
+        		attackDif = 4;
+        	}
+        	//Stage 3 - 3 bats, faster fireballs
+        	if(health <= 4 && stage2 == 0)
+        	{
+        		Instantiate (bat, transform.position, Quaternion.identity);
+        		Instantiate (bat, transform.position, Quaternion.identity);
+        		Instantiate (bat, transform.position, Quaternion.identity);
+        		stage2 = 1;
+        		//More fireballs!
+        		attackDif = 3.5;
+        	}
+        }
+	}
+}
+
+function Die () {
+	Destroy (gameObject);
+}
---
+++ Assets/Scripts/Fireball.js
@@ -0,0 +1,94 @@
+#pragma strict
+//Credit: JamesLeeNZ
+ 
+
+var _velocity : float = 7;
+
+var _torque: float = 1.5;
+
+var _target : Transform;
+
+var _rigidbody : Rigidbody;
+
+var destroyTimer = 0.0;
+
+ 
+
+function Start()
+
+{
+
+  _rigidbody = transform.rigidbody;
+  
+  destroyTimer = Time.time;
+
+  Fire();
+
+}
+
+ 
+
+function Fire()
+
+{
+
+   var distance = Mathf.Infinity;
+
+   for (var go : GameObject in GameObject.FindGameObjectsWithTag("Player"))  { 
+		
+
+      var diff = (go.transform.position - transform.position).sqrMagnitude;   
+
+ 
+
+      if(diff < distance)
+
+      {
+
+         distance = diff;
+
+         _target =  go.transform;
+
+ 
+
+      }
+
+   }
+
+}
+
+ 
+
+function FixedUpdate()
+
+{
+	if(Time.time - destroyTimer > 7)
+	{
+		Destroy(gameObject);
+	}
+ 
+	transform.position.z = 0;
+  if(_target == null || _rigidbody == null)
+	{
+		print("null target: "+_target.tag+" Rigid: "+_rigidbody.tag);
+     return;
+	}
+    _rigidbody.velocity = transform.forward * _velocity;
+
+ 
+
+    var targetRotation = Quaternion.LookRotation(_target.position - transform.position);
+
+    _rigidbody.MoveRotation(Quaternion.RotateTowards(transform.rotation, targetRotation, _torque));
+
+}
+
+ 
+
+function OnCollisionEnter(collision : Collision)
+
+{
+
+   Destroy(gameObject);
+
+}
