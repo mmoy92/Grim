aa4ab647d3f21996232f08ba3467ae131b07d686
stef5 <stef5@vt.edu>
2014-05-04 19:50:13 -0400

    Upgrade scripts for dash and special attack. Also hacky video player from images & sound file so we can play movies without unity pro

---
+++ Assets/DashUpgradeScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpgradeScript : MonoBehaviour {
+	public GUIText guiText;
+	private bool entered = false;
+
+	void OnTriggerEnter(Collider other) {
+		if (entered || other.name != "Player")
+			return;
+		entered = true;
+		StartCoroutine(DisplayText());
+		PlatformerPhysics phys = other.gameObject.GetComponent<PlatformerPhysics>();
+		grimInfo info = other.gameObject.GetComponent<grimInfo>();
+		if (info.soulCount < 0) { // good
+			phys.hasEvilDash = false;
+			phys.hasGoodDash = true;
+		} else { // bad
+			phys.hasEvilDash = true;
+			phys.hasGoodDash = false;
+		}
+	}
+
+	IEnumerator DisplayText() {
+		guiText.text = "Congratulations!\nYou have unlocked: Dash.\nPress Shift to Dash";
+		yield return new WaitForSeconds(5);
+		guiText.text = "";
+	}
+}
---
+++ Assets/SpeAttackUpgraderer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpeAttackUpgraderer : MonoBehaviour {
+	public GUIText guiText;
+	private bool entered = false;
+	
+	void OnTriggerEnter(Collider other) {
+		if (entered || other.name != "Player")
+			return;
+		entered = true;
+		StartCoroutine(DisplayText());
+		PlatformerPhysics phys = other.gameObject.GetComponent<PlatformerPhysics>();
+		grimInfo info = other.gameObject.GetComponent<grimInfo>();
+
+		if (info.soulCount < 0) { // good
+			phys.hasEvilAttack = false;
+			phys.hasGoodAttack = true;
+		} else { // bad
+			phys.hasEvilAttack = true;
+			phys.hasGoodAttack = false;
+		}
+	}
+	
+	IEnumerator DisplayText() {
+		guiText.text = "Congratulations!\nYou have unlocked a new attack.\nRight Click for special attack";
+		yield return new WaitForSeconds(5);
+		guiText.text = "";
+	}
+}
