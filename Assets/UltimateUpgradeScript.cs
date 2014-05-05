b906e8e53ad96c0d6f5ecd753379ef977867d00b
Michael Moy <mikedmoy@gmail.com>
2014-05-05 12:30:57 -0400

    Level 2 changes

--- Assets/UltimateUpgradeScript.cs
+++ Assets/UltimateUpgradeScript.cs
@@ -2,32 +2,29 @@
 using System.Collections;
 
 public class UltimateUpgradeScript : MonoBehaviour {
-	public GUIText guiText;
+	public Vector3 location;
+	public GameObject goodSkully;
+	public GameObject badSkully;
 	private bool entered = false;
 	
 	void OnTriggerEnter(Collider other) {
-		if (entered || other.name != "Player")
+		if (entered || other.tag != "Player")
 			return;
 		entered = true;
-		StartCoroutine(DisplayText());
 		PlatformerPhysics phys = other.gameObject.GetComponent<PlatformerPhysics>();
 		grimInfo info = other.gameObject.GetComponent<grimInfo>();
 		if (info.soulCount < 0) { // good
 						phys.hasEvilAttack = false;
 						phys.hasGoodAttack = true;
+						Instantiate(goodSkully, location, Quaternion.Euler(new Vector3(0,0,0)));
 						PlayerPrefs.SetInt ("evilAttack", 0);
 						PlayerPrefs.SetInt ("goodAttack", 1);
 				} else { // bad
 						phys.hasEvilAttack = true;
 						phys.hasGoodAttack = false;
+						Instantiate(badSkully, location, Quaternion.Euler(new Vector3(0,0,0)));
 						PlayerPrefs.SetInt ("evilAttack", 1);
 						PlayerPrefs.SetInt ("goodAttack", 0);
 				}
-	}
-	
-	IEnumerator DisplayText() {
-	    guiText.text = "Congratulations!\nYou have unlocked a new attack.\nRight Click for special attack";		yield return new WaitForSeconds(5);
-		yield return new WaitForSeconds(5);
-	    guiText.text = "";
 	}
 }
