204fc918581f6c5a1929060eca6df99a39b41802
Michael Moy <mikedmoy@gmail.com>
2014-05-05 12:10:02 -0400

    Level1 changes

--- Assets/DashUpgradeScript.cs
+++ Assets/DashUpgradeScript.cs
@@ -2,32 +2,29 @@
 using System.Collections;
 
 public class DashUpgradeScript : MonoBehaviour {
-	public GUIText guiText;
 	private bool entered = false;
-
+	public Vector3 location;
+	public GameObject goodSkully;
+	public GameObject badSkully;
 	void OnTriggerEnter(Collider other) {
-		if (entered || other.name != "Player")
+		if (entered || other.tag != "Player")
 			return;
 		entered = true;
-		StartCoroutine(DisplayText());
 		PlatformerPhysics phys = other.gameObject.GetComponent<PlatformerPhysics>();
 		grimInfo info = other.gameObject.GetComponent<grimInfo>();
 		if (info.soulCount < 0) { // good
 			phys.hasEvilDash = false;
 			phys.hasGoodDash = true;
+			Instantiate(goodSkully, location, Quaternion.Euler(new Vector3(0,0,0)));
 			PlayerPrefs.SetInt("evilDash", 0);
 			PlayerPrefs.SetInt("goodDash", 1);
 		} else { // bad
 			phys.hasEvilDash = true;
 			phys.hasGoodDash = false;
+			Instantiate(badSkully, location, Quaternion.Euler(new Vector3(0,0,0)));
 			PlayerPrefs.SetInt("evilDash", 1);
 			PlayerPrefs.SetInt("goodDash", 0);
 		}
 	}
 
-	IEnumerator DisplayText() {
-		guiText.text = "Congratulations!\nYou have unlocked: Dash.\nPress Shift to Dash";
-		yield return new WaitForSeconds(5);
-		guiText.text = "";
-	}
 }
