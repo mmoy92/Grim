e0bf88ab863be6f4e9eb2bc35a9252069675ea87
stef5 <stef5@vt.edu>
2014-05-05 16:03:27 -0400

    ditto

---
+++ Assets/moviescript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moviescript : MonoBehaviour {
+	public MovieTexture movie;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(StartMovie());
+	}
+	IEnumerator StartMovie() {
+		while(!movie.isReadyToPlay)
+			yield return null;
+		renderer.material.mainTexture = movie;
+		Debug.Log ("play movie");
+		movie.Play();
+		audio.Play();
+
+		yield return new WaitForSeconds(36.0f);
+		Debug.Log ("Load next level");
+		Application.LoadLevel(1);
+	}
+	// Update is called once per frame
+	void Update () {
+		movie.Play();
+	}
+}
